72afa69e25954757e7c769a39a66840af4751a80
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2024-08-20 13:29:28 +0200

    implement starting state picking logic

--- src/logic.py
+++ src/logic.py
@@ -1,1 +1,29 @@
+from typing import Callable, Any, Iterable
+import numpy as np
 
+type State = Any  # TODO: placeholder, replace it
+type Number = float | int | np.number  # TODO: add more?
+
+# +------------------------+
+# | Starting State Picking |
+# +------------------------+
+
+def pick_starting_state(value_function: Callable[[State], Number], novelty_function: Callable[[State], Number], state_candidates: Iterable[State], beta_proximal: Number, gamma_tradeoff: Number) -> State:
+    state_candidates = np.array(state_candidates)
+    assert len(state_candidates.shape) == 1  # only list of states
+    # Calculate distribution over starting states based on probability of success
+    state_pos = np.array([beta_proximal * value * (1 - value) for value in map(value_function, state_candidates)])
+    pos_dist = state_pos / np.sum(state_pos)
+
+    # Calculate distribution over starting states based on state novelty
+    state_novelty = np.array([novelty_function(state) for state in state_candidates])
+    novelty_dist = state_novelty / np.sum(state_novelty)
+
+    combined_dist = gamma_tradeoff * pos_dist + (1 - gamma_tradeoff) * novelty_dist
+
+    return np.random.choice(state_candidates, p=combined_dist)
+
+
+# +--------------------------+
+# | State Novelty Approaches |
+# +--------------------------+
