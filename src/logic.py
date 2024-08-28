1c2afb14f14223464e54eab5c0adb6245f2daed9
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2024-08-28 11:50:07 +0200

    add function to map state to observation

--- src/logic.py
+++ src/logic.py
@@ -1,5 +1,6 @@
 from typing import Callable, Any, Iterable
 import numpy as np
+from gymnasium.core import ObsType
 
 type State = Any  # TODO: placeholder, replace it
 type Number = float | int | np.number  # TODO: add more?
@@ -8,11 +9,11 @@ type Number = float | int | np.number  # TODO: add more?
 # | Starting State Picking |
 # +------------------------+
 
-def pick_starting_state(value_function: Callable[[State], Number], novelty_function: Callable[[State], Number], state_candidates: Iterable[State], beta_proximal: Number, gamma_tradeoff: Number) -> State:
+def pick_starting_state(value_function: Callable[[State], Number], novelty_function: Callable[[State], Number], state_candidates: Iterable[State], state_to_obs: Callable[[State], ObsType], beta_proximal: Number, gamma_tradeoff: Number) -> State:
     state_candidates = np.array(state_candidates)
     assert len(state_candidates.shape) == 1  # only list of states
     # Calculate distribution over starting states based on probability of success
-    state_pos = np.array([beta_proximal * value * (1 - value) for value in map(value_function, state_candidates)])
+    state_pos = np.array([beta_proximal * value * (1 - value) for value in map(value_function, map(state_to_obs, state_candidates))])
     pos_dist = state_pos / np.sum(state_pos)
 
     # Calculate distribution over starting states based on state novelty
