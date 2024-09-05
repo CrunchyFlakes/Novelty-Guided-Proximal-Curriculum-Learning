b498e27caeebebe96fe7312e64e06e7118431539
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2024-09-06 00:17:03 +0200

    run script updates

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@
 #SBATCH --mem=32G
 #SBATCH -o slurm_outputs/slurm-%j.out
 #SBATCH --partition=ai
-#SBATCH --time=6:00:00
+#SBATCH --time=12:00:00
 
 #SBATCH --mail-user=m.toepperwien@stud.uni-hannover.de
 #SBATCH --mail-type=FAIL,END
@@ -19,4 +19,4 @@ conda activate /bigwork/nhwptopm/.conda/envs/adrl
 
 export WANDB_MODE=offline
 
-python /bigwork/nhwptopm/adrl_proximal_curriculum_w_novelty/main.py --workers=20 --trials=100 --env_name doorkey --env_size 8 --n_seeds_train 2 --n_seeds_eval 1 --smac_output_dir smac3_output/${SLURM_JOB_ID}_${SLURM_JOB_NAME}
+python /bigwork/nhwptopm/adrl_proximal_curriculum_w_novelty/main.py --workers=20 --trials=100 --env_name doorkey --env_size 8 --n_seeds_train 1 --n_seeds_eval 1 --smac_output_dir smac3_output/${SLURM_JOB_ID}_${SLURM_JOB_NAME}
