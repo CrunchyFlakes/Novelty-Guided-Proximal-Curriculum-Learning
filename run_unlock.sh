99f1edad7fa12e26066fa607bc1aed6b35208e80
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2024-09-05 21:56:32 +0200

    run script updates

--- run_unlock.sh
+++ run_unlock.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=20
-#SBATCH -J doorkey8
+#SBATCH -J unlock
 #SBATCH --mem=32G
 #SBATCH -o slurm_outputs/slurm-%j.out
 #SBATCH --partition=ai
@@ -19,4 +19,4 @@ conda activate /bigwork/nhwptopm/.conda/envs/adrl
 
 export WANDB_MODE=offline
 
-python /bigwork/nhwptopm/adrl_proximal_curriculum_w_novelty/main.py --workers=20 --trials=100 --env_name doorkey --env_size 8 --n_seeds_train 2 --n_seeds_eval 1 --smac_output_dir smac3_output/${SLURM_JOB_ID}_${SLURM_JOB_NAME}
+python /bigwork/nhwptopm/adrl_proximal_curriculum_w_novelty/main.py --workers=20 --trials=100 --env_name unlock --env_size 5 --n_seeds_train 2 --n_seeds_eval 1 --smac_output_dir smac3_output/${SLURM_JOB_ID}_${SLURM_JOB_NAME}
