875d1d593a272401adc96576e76217690222612f
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2024-09-06 14:38:50 +0200

    adjust default hyperparameters and restore batch size as hp

--- src/hpo.py
+++ src/hpo.py
@@ -2,16 +2,16 @@ from ConfigSpace import ConfigurationSpace, Float, Integer, Constant, EqualsCond
 
 
 def get_ppo_config_space(use_prox_curr: bool = True, use_state_novelty: bool = True) -> ConfigurationSpace:
-    # Default values are taken from stable baselines ppo algorithm
+    # Default values are mostly taken from stable baselines ppo algorithm
     # This part only covers the stable_baselines hyperparemeters
     configspace_sb_ppo =  ConfigurationSpace({
         # number of steps to run environment before update
         #"n_steps": Integer("n_steps", (2, 8192), default=2048),
-        #"batch_size": Integer("batch_size", (1, 512), default=64),
-        "gamma": Float("gamma", (0.9, 1.0), default=0.99),
+        "batch_size": Categorical("batch_size", (16, 32, 64, 128, 256), default=64),
+        "gamma": Float("gamma", (0.9, 1.0), default=0.95),
         "gae_lambda": Float("gae_lambda", (0.5, 1.0), default=0.95),
         "clip_range": Float("clip_range", (0.0, 1.0), default=0.2),
-        "ent_coef": Float("ent_coef", (0.0, 1.0), default=0.0),
+        "ent_coef": Float("ent_coef", (0.0, 1.0), default=0.02),  # differs from stable baselines default, more exploration
         "vf_coef": Float("vf_coef", (0.0, 1.0), default=0.5),
         "max_grad_norm": Float("max_grad_norm", (0.0, 1.0), default=0.5),
         # "use_sde": ..., don't use this for now, have to read paper first. This may be cut due to time constraints
@@ -28,7 +28,7 @@ def get_ppo_config_space(use_prox_curr: bool = True, use_state_novelty: bool = T
     ### layer sizes
     max_layers = 5
     policy_n_layers = Integer("policy_n_layers", (1, max_layers), default=2)
-    policy_layersizes = [Integer(f"policy_layer{i}_size", (1, 128), default=32) for i in range(max_layers)]
+    policy_layersizes = [Integer(f"policy_layer{i}_size", (1, 128), default=64) for i in range(max_layers)]
     policy_layersize_conds = [GreaterThanCondition(policy_layersizes[i], policy_n_layers, i) for i in range(1, max_layers)]
     configspace_sb_policy.add((policy_n_layers, *policy_layersizes, *policy_layersize_conds))
  
@@ -40,7 +40,7 @@ def get_ppo_config_space(use_prox_curr: bool = True, use_state_novelty: bool = T
         "use_state_novelty": Constant("use_state_novelty", str(use_state_novelty))
     })
     if use_prox_curr:
-        beta_proximal = Float("beta_proximal", (0.0, 1.0), default=0.5)  # this default is guessed
+        beta_proximal = Float("beta_proximal", (0.0, 1.0), default=0.75)
     else:
         beta_proximal = Constant("beta_proximal", 0.0)
     configspace_approach.add(beta_proximal)
@@ -62,7 +62,7 @@ def get_ppo_config_space(use_prox_curr: bool = True, use_state_novelty: bool = T
         ## Random Network Distillation
         rnd_loss = Constant("rnd_loss", "mse")
         rnd_loss_cond = EqualsCondition(rnd_loss, novelty_approach, "rnd")
-        rnd_activation = Categorical("rnd_activation", ("relu", "leakyrelu"), default="relu")
+        rnd_activation = Categorical("rnd_activation", ("relu", "leakyrelu"), default="leakyrelu")
         rnd_activation_cond = EqualsCondition(rnd_activation, novelty_approach, "rnd")
         rnd_learning_rate = Float("rnd_learning_rate", (1.0e-6, 1.0), default=1e-3, log=True)
         rnd_learning_rate_cond = EqualsCondition(rnd_learning_rate, novelty_approach, "rnd")
@@ -71,8 +71,8 @@ def get_ppo_config_space(use_prox_curr: bool = True, use_state_novelty: bool = T
         configspace_approach.add((rnd_loss, rnd_loss_cond, rnd_activation, rnd_activation_cond, rnd_learning_rate, rnd_learning_rate_cond, rnd_optimizer, rnd_optimizer_cond,))
         ### layer sizes
         max_layers = 5
-        rnd_n_layers = Integer("rnd_n_layers", (1, max_layers), default=2)
-        rnd_layersizes = [Integer(f"rnd_layer{i}_size", (1, 128), default=32) for i in range(max_layers)]
+        rnd_n_layers = Integer("rnd_n_layers", (1, max_layers), default=3)
+        rnd_layersizes = [Integer(f"rnd_layer{i}_size", (1, 128), default=default_size) for i, default_size in zip(range(max_layers), [128, 64, 32] + [32] * (max_layers - 3))]
         rnd_layersize_conds = [GreaterThanCondition(rnd_layersizes[i], rnd_n_layers, i) for i in range(1, max_layers)]
         configspace_approach.add((rnd_n_layers, *rnd_layersizes, *rnd_layersize_conds))
     
