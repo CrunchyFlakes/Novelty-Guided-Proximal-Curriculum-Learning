de4b99487661005f1cb1c51f8b735ec17df5edc3
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2024-09-09 17:54:26 +0200

    document logic

--- src/logic.py
+++ src/logic.py
@@ -9,8 +9,8 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-type State = Any  # TODO: placeholder, replace it
-type Number = float | int | np.number  # TODO: add more?
+type State = Any
+type Number = float | int | np.number
 
 # +------------------------+
 # | Starting State Picking |
@@ -26,6 +26,21 @@ def pick_starting_state(
     beta_novelty: Number,
     gamma_tradeoff: Number,
 ) -> tuple:
+    """Main logic of novelty-guided proximal curriculum learning. Samples from given states
+    Also implements proximal curriculum learning ([Tzannetos et al. 2023])
+
+    Args:
+        value_function: value function of the agent
+        novelty_function: novelty function which maps states to novelty
+        state_candidates: states that may serve as starting states
+        state_to_obs: function to generate observations from states
+        beta_proximal: proximal curriculum beta parameter -> smoothness/exaggeration of distribution
+        beta_novelty: novelty approach beta parameter -> smoothness/exaggeration of distribution
+        gamma_tradeoff: hyperparameter to combine calculated distributions for proximal curriculum and state novelty
+
+    Returns:
+        starting state
+    """
     state_candidates = list(state_candidates)
     states_as_obs = list(map(state_to_obs, state_candidates))
     # Calculate distribution over starting states based on probability of success
@@ -68,6 +83,13 @@ def pick_starting_state(
 
 # Random Network Distillation
 class RND:
+    """Class implementing Random Network Distillation ([Burda et al., 2018])
+
+    Attributes:
+        layers_fixed: layer sizes for the fixed network
+        layers_learned: layer sizes for the approximating network
+    """
+
     def __init__(
         self,
         layers_fixed: list[int],
@@ -77,6 +99,21 @@ class RND:
         activation_function: str,
         optimizer: str,
     ):
+        """Initialize random network distillation
+
+        Args:
+            layers_fixed: layer sizes of the fixed network
+            layers_learned: layer sizes of the approximating network
+            loss: loss to use
+            learning_rate: learning rate for approximating network
+            activation_function: activation function to use
+            optimizer: optimizer to use
+
+        Raises:
+            NotImplementedError: Given unknown activation function
+            NotImplementedError: Given unknown optimizer
+            NotImplementedError: Given unknown loss
+        """
         match activation_function.lower():
             case "relu":
                 activation_class = nn.ReLU
@@ -115,8 +152,21 @@ class RND:
         match optimizer.lower():
             case "adam":
                 self.optimizer = torch.optim.Adam(self.layers_learned.parameters(), lr=learning_rate)  # type: ignore  # unresolved import bug in pytorch, only affects pyright (lsp)
+            case _:
+                raise NotImplementedError(f'optimizer "{optimizer}" not implemented')
 
     def __call__(self, input: torch.Tensor, learn_network: bool = True) -> torch.Tensor:
+        """Calculates state novelty for each given observation.
+
+        May also learn the network.
+
+        Args:
+            input: states/observations to calculate novelty over
+            learn_network: if this should count as seen by the agent and therefore the network should learn
+
+        Returns:
+            state novelty scores
+        """
         self.optimizer.zero_grad()
 
         with torch.no_grad():
